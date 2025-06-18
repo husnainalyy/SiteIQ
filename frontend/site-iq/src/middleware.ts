c7ff89d80a3a58839e2a2c61e42a2632863e1143
Mabdu11ah-abd <iamabdullahforu@gmail.com>
2025-06-18 20:22:56 +0500

    much much stuff done

--- frontend/site-iq/src/middleware.ts
+++ frontend/site-iq/src/middleware.ts
@@ -1,28 +1,10 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/features(.*)",
-    "/pricing(.*)",
-    "/aboutus(.*)",
-    "/api/webhook(.*)",
-    "/success(.*)",
-    "/cancel(.*)"
-  ],
-  afterAuth(auth, req, evt) {
-    // Handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      const signInUrl = new URL('/sign-in', req.url);
-      signInUrl.searchParams.set('redirect_url', req.url);
-      return Response.redirect(signInUrl);
-    }
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect()
   }
-});
+  })
 
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
